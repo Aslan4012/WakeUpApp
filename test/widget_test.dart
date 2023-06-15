f0be0e8197ebaae460a47650de896fd062c42294
mart994j <martinjespersen9@gmail.com>
2023-06-15 14:04:13 +0200

    lol

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:wakeup/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -24,7 +24,7 @@ void main() {
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
 }
